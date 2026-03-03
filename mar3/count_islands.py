4b6e28c30633a7e2c43007a1ef2db060d6022a94
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-03 22:16:13 +0530

    commit 5

---
+++ mar3/count_islands.py
@@ -0,0 +1,24 @@
+def num_islands(grid):
+    def dfs(r, c):
+        if r<0 or c<0 or r>=rows or c>=cols or grid[r][c] == "0":
+            return
+        grid[r][c] = "0"
+        dfs(r+1,c); dfs(r-1,c)
+        dfs(r,c+1); dfs(r,c-1)
+
+    rows, cols = len(grid), len(grid[0])
+    count = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "1":
+                dfs(r,c)
+                count += 1
+    return count
+
+print(num_islands([
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+]))
