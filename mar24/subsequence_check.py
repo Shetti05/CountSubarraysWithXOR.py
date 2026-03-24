21b45a593016e2183c02e53e931b66e4486d6a39
Jaideep <jaideepshetti55@gmail.com>
2026-03-24 20:11:52 +0530

    commit 13

---
+++ mar24/subsequence_check.py
@@ -0,0 +1,10 @@
+# File: subsequence_check.py
+def is_subsequence(s, t):
+    i = j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
+
+print(is_subsequence("abc", "ahbgdc"))
