82b1232d619ff7bdbefd1bb7742d45e96f254144
Jaideep <jaideepshetti55@gmail.com>
2026-03-27 22:55:36 +0530

    commit 5

---
+++ mar27/kth_largest.py
@@ -0,0 +1,9 @@
+import heapq
+
+def kth_largest(nums, k):
+    heap = []
+    for n in nums:
+        heapq.heappush(heap, n)
+        if len(heap) > k:
+            heapq.heappop(heap)
+    return heap[0]
